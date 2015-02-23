4e5545e0a4cfd864d1b208312ab158e494d7e1bd
blkredstarV <blkredstar5@gmail.com>
2015-02-22 22:46:43 -0600

    broke it

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,14 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/d2-common/include
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
-TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
+TARGET_KERNEL_CONFIG        := broken_d2_defconfig
+
+# Custom Kernel Toolchains
+TARGET_GCC_VERSION_AND := 4.9-linaro
+TARGET_GCC_VERSION_ARM := 4.9-linaro
+
+# Broken Optimizations
+BROKEIT := true
 
 # Audio
 BOARD_HAVE_AUDIENCE_A2220 := true
