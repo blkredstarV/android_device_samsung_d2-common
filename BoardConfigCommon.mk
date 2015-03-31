4dda62bb2a31b078d953c8a5fa9b5218f3c21d29
blkredstarV <blkredstar5@gmail.com>
2015-03-31 18:19:04 -0500

    no TCs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,9 +32,6 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 TARGET_KERNEL_CONFIG        := broken_d2_defconfig
 
 # Custom Kernel Toolchains
-KERNEL_TOOLCHAIN_PREFIX := arm-cortex_a7-linux-gnueabihf-
-KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-arm-cortex_a7-linux-gnueabihf-linaro_4.9/bin"
-TARGET_GCC_VERSION_AND := arm-cortex_a7-linux-gnueabihf-linaro_4.9
 
 # Broken Optimizations
 BROKEIT := true
