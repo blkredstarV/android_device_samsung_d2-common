9da0a510a2705a3a3726a0dba6a820713b88eeb1
Roberto Correa <blkredstar5@gmail.com>
2015-11-25 09:45:50 -0600

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/d2-common/include
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
-TARGET_KERNEL_CONFIG        := deathly_d2_defconfig
+TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
 
 # Custom Kernel Toolchains
 KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin/"
